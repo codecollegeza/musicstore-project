aeb947c3858d404ec7d025102a36de41e7b7e89e
TigerFoxBlack <t.hibbins@gmail.com>
2016-06-24 08:52:28 +0200

    Added a Product Service

--- src/main/java/com/compuways/service/DefaultProductService.java
+++ src/main/java/com/compuways/service/DefaultProductService.java
@@ -1,7 +1,42 @@
 package com.compuways.service;
 
-/**
- * Created by Tiger on 2016/06/24.
- */
-public class DefaultProductService {
+import com.compuways.domain.Product;
+import com.compuways.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class DefaultProductService implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product updateProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product selectProduct(String code) {
+        return productRepository.findByCode(code);
+    }
+
+    @Override
+    public Product selectProduct(long productId) {
+        return productRepository.findOne(productId);
+    }
+
+    @Override
+    public List<Product> selectProducts() {
+        return productRepository.findAll();
+    }
 }
